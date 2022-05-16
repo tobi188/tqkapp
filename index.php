18d75dd152d50ee15b2c90357b6091ff82971c2b
tqkhanh2 <quockhanhto93@gmail.com>
2022-05-16 16:18:00 +0900

    test connect to aws rdb

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
-    echo "hello khanh";
+    $servername = "tqkdb.cegoxxvpdnqu.ap-northeast-1.rds.amazonaws.com";
+    $username = "admin";
+    $password = "Toquockhanh93";
+    
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password);
+    
+    // Check connection
+    if (!$conn) {
+      die("Connection failed: " . mysqli_connect_error());
+    }
+    echo "Connected successfully";
 ?>
